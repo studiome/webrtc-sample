29a9590f1f4f70a2d141f6bdf5cf5771e2f9d2c0
Kazuhiro Miyahara <kazuhiro.miyahara.vs@gmail.com>
2021-08-04 23:50:12 +0900

    proper timing on lifecycle

--- src/app/vidoeview/vidoeview.component.ts
+++ src/app/vidoeview/vidoeview.component.ts
@@ -1,19 +1,20 @@
-import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
+import { Component, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
 
 @Component({
   selector: 'app-vidoeview',
   templateUrl: './vidoeview.component.html',
   styleUrls: ['./vidoeview.component.scss']
 })
-export class VidoeviewComponent implements OnInit {
-  @ViewChild('videoview') cameraView!: ElementRef<HTMLVideoElement>;
+export class VidoeviewComponent implements AfterViewInit {
+  @ViewChild('videoview') cameraView?: ElementRef<HTMLVideoElement>;
   constructor() { }
 
-
-  ngOnInit(): void {
-    const self: this = this;
-    navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then(function (stream) {
-      self.cameraView.nativeElement.srcObject = stream;
-    });
+  ngAfterViewInit(): void {
+    let _cv = this.cameraView?.nativeElement;
+    if (_cv) {
+      navigator.mediaDevices.getUserMedia({ video: true, audio: false }).then(function (stream) {
+        _cv!.srcObject = stream;
+      });
+    }
   }
 }
